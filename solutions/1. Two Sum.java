dcfec9190f2a21c6fd4c03102c04e9e1bee13e62
Bhupendra Singh Gadwal <60140629+backdoor20@users.noreply.github.com>
2021-01-14 10:35:44 +0530

    0 ms

--- solutions/1. Two Sum.java
+++ solutions/1. Two Sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    int[] ans=new int[2];
-                    ans[0]=i;ans[1]=j;
-                    return ans;
+        HashMap<Integer,Integer> map=new HashMap<>();
+         for(int i=0;i<nums.length;i++){
+             int comp=target-nums[i];
+            if(map.containsKey(comp)){
+                return new int[]{map.get(comp),i};             
                 }
-            }
+            
+             map.put(nums[i],i);
         }
         return new int[1];
     }
