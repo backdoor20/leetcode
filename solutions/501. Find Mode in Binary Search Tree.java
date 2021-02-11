d73e1d702580069bcde6f0d9a6b1ae26925bf9a0
Bhupendra Singh Gadwal <60140629+backdoor20@users.noreply.github.com>
2021-02-11 20:22:23 +0530

    1 ms

--- solutions/501. Find Mode in Binary Search Tree.java
+++ solutions/501. Find Mode in Binary Search Tree.java
@@ -1,28 +1,45 @@
-                number=a;
-                max=map.get(a);
-            }
-        }
-        int count=0;
-        for(Integer a:map.keySet()){
-            if(map.get(a)==max){
-                count++;
-            }
-        }
-        int[] arr=new int[count];
-        count=0;
-        for(Integer a:map.keySet()){
-            if(map.get(a)==max){
-                arr[count++]=a;
-            }
-        }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxCount=-1;
+    int lastValue=Integer.MIN_VALUE;
+    int currentCount=0;
+    List<Integer> list=new ArrayList<>();
+    public int[] findMode(TreeNode root) {
+        inorder(root);
+        int[] arr=new int[list.size()];
+        int i=0;
+        for(Integer a: list)
+        arr[i++]=a;
         return arr;
     }
     
-    public void findFrequency(TreeNode root,Map<Integer,Integer> map){
+    public void inorder(TreeNode root){
         if(root==null)
             return;
-        findFrequency(root.left,map);
-        map.put(root.val,map.getOrDefault(root.val,0)+1);
-        findFrequency(root.right,map);
+        inorder(root.left);
+        processCurrent(root.val);
+        inorder(root.right);
+    }
+    public void processCurrent(int value){
+        if(lastValue!=value){
+            lastValue=value;
+            currentCount=0;
+        }
+        currentCount++;
+        if(currentCount>maxCount){
+            list=new ArrayList();
+            list.add(value);
+            maxCount=currentCount;
+        }else if(currentCount==maxCount){
+            list.add(value);
+        }
     }
 }
